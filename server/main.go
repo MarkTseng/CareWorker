1eca9275234957007ce766aa0b8f7b8af726eef4
Mark Tseng <chunjen.tseng@gmail.com>
2019-04-15 15:04:38 +0800

    [BF] fixed compile error [M] add app debug option

--- server/main.go
+++ server/main.go
@@ -135,7 +135,7 @@ func main() {
 
 	// Process the templates at the start so that they don't have to be loaded
 	// from the disk again. This makes serving HTML pages very fast.
-	cws.router.LoadHTMLGlob("public/templates/*")
+	//cws.router.LoadHTMLGlob("public/templates/*")
 
 	// Initialize the routes
 	initializeRoutes(cws)
